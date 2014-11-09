9c5454653e2bbc72bd359a42df2e336dd61ddff1
Vince Lugli <vince.lugli@gmail.com>
2014-11-08 19:53:43 -0500

    Added signin page with some error messages

--- Lynx/gen/com/lynx/R.java
+++ Lynx/gen/com/lynx/R.java
@@ -1091,23 +1091,24 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050043;
+        public static final int action_settings=0x7f05004a;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003f;
-        public static final int button2=0x7f050040;
+        public static final int button1=0x7f05003d;
+        public static final int button2=0x7f05003e;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
-        public static final int editText1=0x7f05003e;
-        public static final int editText2=0x7f05003d;
+        public static final int editText1=0x7f050040;
+        public static final int editText2=0x7f05003f;
         public static final int editText3=0x7f050041;
         public static final int editText4=0x7f050042;
         public static final int edit_query=0x7f050030;
+        public static final int email_login_form=0x7f050045;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
@@ -1118,10 +1119,14 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int login=0x7f050048;
+        public static final int login_form=0x7f050044;
+        public static final int login_progress=0x7f050043;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int password=0x7f050047;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1147,6 +1152,8 @@ containing a value of this type.
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int username=0x7f050046;
+        public static final int username_sign_in_button=0x7f050049;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1199,9 +1206,9 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_login=0x7f030018;
-        public static final int activity_main=0x7f030019;
-        public static final int activity_register=0x7f03001a;
+        public static final int activity_main=0x7f030018;
+        public static final int activity_register=0x7f030019;
+        public static final int activity_sing_in=0x7f03001a;
         public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
@@ -1251,13 +1258,30 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
+        public static final int action_sign_in=0x7f0a0019;
+        public static final int action_sign_in_short=0x7f0a001a;
         public static final int app_name=0x7f0a000d;
         public static final int confirm_password=0x7f0a0015;
         public static final int display_name=0x7f0a0014;
+        public static final int error_field_required=0x7f0a0020;
+        public static final int error_incorrect_password=0x7f0a001f;
+        public static final int error_invalid_email=0x7f0a0023;
+        public static final int error_invalid_password=0x7f0a001e;
+        public static final int error_invalid_username=0x7f0a001d;
         public static final int login=0x7f0a0012;
         public static final int password=0x7f0a0011;
+        public static final int plus_disconnect=0x7f0a001c;
+        public static final int plus_sign_out=0x7f0a001b;
+        /**  Strings related to login 
+ Strings related to login 
+         */
+        public static final int prompt_email=0x7f0a0017;
+        public static final int prompt_password=0x7f0a0018;
+        public static final int prompt_username=0x7f0a0021;
         public static final int register=0x7f0a0013;
         public static final int title=0x7f0a000e;
+        public static final int title_activity_main=0x7f0a0016;
+        public static final int title_activity_sing_in=0x7f0a0022;
         public static final int username=0x7f0a0010;
     }
     public static final class style {
